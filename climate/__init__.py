d694fa10a02e8e22b1f20ea47f841cb8ba776aad
Taffik <Taff1k.tpl@gmai.com>
2026-01-16 14:42:24 +0300

    add __init__.py and delete trash

---
+++ climate/__init__.py
@@ -0,0 +1,6 @@
+from .app import App
+from .page.page import Page, InD, RD
+from .page.page_data import PageData
+from .page.reverse_data import ReverseData
+
+__all__ = ["App", "Page", "PageData", "ReverseData", "InD", "RD"]
